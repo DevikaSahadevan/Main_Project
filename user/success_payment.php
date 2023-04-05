a4a0e78d6a08b049eb2fe11d61e50d6a4bb4c211
DevikaSahadevan <devikasahadevan2023a@mca.ajce.in>
2023-04-05 09:22:26 +0530

    Payment success

--- user/success_payment.php
+++ user/success_payment.php
@@ -3,5 +3,5 @@ include '../dbconnect.php';
 $pay_id = $_POST['payment_id'];
 $amount = $_POST['amt'];
 $user = $_POST['user'];
-$pay_query = mysql_query("INSERT INTO `tbl_payment`(`transaction_id`, `amount`, `user`) VALUES('$pay_id', '$amount', '$user')");
+$pay_query = mysql_query("INSERT INTO `tbl_payment`(`transaction_id`, `amount`, `user`,`pay_date`) VALUES('$pay_id', '$amount', '$user',NOW())");
 ?>
